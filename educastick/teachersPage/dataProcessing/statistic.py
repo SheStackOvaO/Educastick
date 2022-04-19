3a9e50df15a64d9c25088d45d9f7d4b746a05f7a
Шестакова Анастасия <stud0000228044@study.utmn.ru>
2022-04-19 20:56:14 +0500

    def statistic

--- educastick/teachersPage/dataProcessing/statistic.py
+++ educastick/teachersPage/dataProcessing/statistic.py
@@ -137,7 +137,7 @@ def chart():
     result = []
     rasultarray = set(rightAnswersSt['right'])
     for question in rasultarray:
-        questions = models.Question.objects.filter(number=question)[0].description
+        #questions = models.Question.objects.filter(number=question)[0].description
         result.append([question, rightAnswersSt['right'].count(question)/len(answers)])
     return plt.plot(result)
 
