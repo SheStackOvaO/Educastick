d970b1150bf358bb7b1fb894c448dcf734935880
Станислав Сбродов <stud0000232373@study.utmn.ru>
2022-05-09 22:36:45 +0500

    Добавил функцию для кнопки загрузки ответов на странице тестов, плюс картинку плюса заменил и добавил картинку возврата

--- educastick/teachersPage/static/js/testsScript.js
+++ educastick/teachersPage/static/js/testsScript.js
@@ -1,40 +1,32 @@
-$(document).ready(function () {
-  let groupsList = ['Группа 1', 'Группа 2']
-
-  $('.element').click(function(){
-    if ($(this).hasClass('active')){
+function MakeBlockActive(){
+  let element_id = $(this).attr('id')
+  if ($(this).hasClass('active')){
       $(this).removeClass('active')
       $(this).next('.active_element').detach()
     } else{
       $(this).addClass('active')
-      let element_id = $(this).attr('id')
-      $(this).after(CreateTestAnswerBlock('Части речи', 'Глаголы', element_id))
+      $(this).after(CreateTestActiveBlock('Части речи', 'Глаголы', element_id))
     }
-  })
+}
 
-
-  $(document).on('click','.active_group_choose', function()
-  {
-      $(this).after(ElementSelectBlock())
-      let datalist = document.querySelector('#list_elements')
-        let datalist2 = $(this).siblings().children('#list_elements')
-        console.log(datalist2)
-        for(let i = 0; i < groupsList.length; i++)
-        {
-          let opt = groupsList[i]
-          let el = document.createElement("option")
-          el.value = opt
-          datalist.appendChild(el)
-        }
-  })
-})
+function AddAnswersButton(){
+   let element_id = $(this).parents('.element').attr('id')
+    if ($(this).parents('.element').hasClass('active')){
+      $(this).parents('.element').next('.active_element').detach()
+      $(this).parents('.element').after(CreateTestAnswerBlock('Части речи', 'Глаголы', element_id))
+    } else{
+      $(this).parents('.element').addClass('active')
+      //console.log(element_id)
+      $(this).parents('.element').after(CreateTestAnswerBlock('Части речи', 'Глаголы', element_id))
+    }
+}
 
 function CreateTestAnswerBlock(module = '', theme = '', element_id){
   return $('<div class="active_element">\n' +
     '        <form action="" name="' + element_id + '">\n' +
     '        <div class="active_text">\n' +
-    '          <p>Модуль: Части речи</p>\n' +
-    '          <p>Тема: Глаголы</p>\n' +
+    '          <p>Модуль: ' + module + '</p>\n' +
+    '          <p>Тема: ' + theme + '</p>\n' +
     '        </div>\n' +
     '        <div class="select_element">\n' +
     '          <div class="element_add active_group_choose">\n' +
@@ -52,6 +44,23 @@ function CreateTestAnswerBlock(module = '', theme = '', element_id){
     '      </div>')
 }
 
+function CreateTestActiveBlock(module = '', theme = '', element_id){
+  return $('<div class="active_element">\n' +
+      '  <a href="" class="statistic_href">Статистика</a>\n' +
+      '        <div class="active_text">\n' +
+      '          <p>Модуль: ' + module + '</p>\n' +
+      '          <p>Тема: ' + theme + '</p>\n' +
+      '          <p>Вопросы:</p>\n' +
+      '          <ul type="none">\n' +
+      '            <li>Вопрос 1</li>\n' +
+      '            <li>Ответ:</li>\n' +
+      '            <li>Вопрос 2</li>\n' +
+      '            <li>Ответ:</li>\n' +
+      '          </ul>\n' +
+      '        </div>\n' +
+      '      </div>')
+}
+
 function ElementSelectBlock(){
   return $('<div class="elements_choose">\n' +
     '            <input list="list_elements" class="find_element">\n' +
@@ -61,8 +70,39 @@ function ElementSelectBlock(){
     '          </div>')
 }
 
+
+$(document).ready(function () {
+  let groupsList = ['Группа 1', 'Группа 2']
+
+  $('.element').click(MakeBlockActive)
+
+  $('.crud_but').click(function (){
+    event.stopPropagation()
+  })
+
+  $('.add_answer_but').click(AddAnswersButton)
+
+  $(document).on('click','.active_group_choose', function()
+  {
+      $(this).after(ElementSelectBlock())
+      let datalist = document.querySelector('#list_elements')
+        let datalist2 = $(this).siblings().children('#list_elements')
+        console.log(datalist2)
+        for(let i = 0; i < groupsList.length; i++)
+        {
+          let opt = groupsList[i]
+          let el = document.createElement("option")
+          el.value = opt
+          datalist.appendChild(el)
+        }
+  })
+
 $(document).on('click','.close_choose', function(){
   $(this).parent().remove()
+})
+
+
+
 })
 
 
